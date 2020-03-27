b15dd43fe87ef43db4e213ba0b7eef3f8846ef88
PandeDarma <62196203+PandeDarma@users.noreply.github.com>
2020-03-27 20:57:01 +0800

    Add files via upload

---
+++ storage/framework/views/636e3e0ef3886d600621eef1930f02a0f335f237.php
@@ -0,0 +1,4 @@
+<?php $__env->startSection('content'); ?>
+<p> ini konten </p>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('LayOut.Awal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\Filenya Pande\blog\resources\views/admin/Kedua.blade.php ENDPATH**/ ?>
